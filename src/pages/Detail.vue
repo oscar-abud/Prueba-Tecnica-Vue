f472ad0d97cd33787140059296afd42d76598254
oscar-abud <oscarpalma.vpr2@gmail.com>
2025-09-02 17:07:14 -0400

    Add: Agregando los componentes Header y Footer a las vistas Detail y List

--- src/pages/Detail.vue
+++ src/pages/Detail.vue
@@ -1,1 +1,10 @@
-<template></template>
+<script setup>
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+</script>
+
+<template>
+  <Header />
+  <main>Hola Mundo</main>
+  <Footer />
+</template>
