97b06e78780727ccfd1c4f341b9046a3fb83ca31
oscar-abud <oscarpalma.vpr2@gmail.com>
2025-09-02 17:03:54 -0400

    Add: Estilos agregados al componente Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,3 +3,14 @@
     <h1>Esto es el footer</h1>
   </footer>
 </template>
+
+<style scoped>
+footer {
+  background-color: var(--gray-color);
+  color: var(--white-color);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+}
+</style>
