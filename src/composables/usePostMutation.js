07a29d7bffe53a1856d7a274255bedbd5057630f
oscar-abud <oscarpalma.vpr2@gmail.com>
2025-09-03 16:22:10 -0400

    Changes: Eliminamos el loadingDelete y el resultDelete, es innecesario.

--- src/composables/usePostMutation.js
+++ src/composables/usePostMutation.js
@@ -6,26 +6,17 @@ import { useUsersStore } from '@/stores/counter'
 export default function usePostMutation() {
   const store = useUsersStore()
 
-  const deleteResult = ref(null)
-
   /* --------------------------------------
     Mutation para eliminar user por id
   -------------------------------------- */
-  const {
-    mutate: deleteUserMutation,
-    loading: deleteLoading,
-    error: deleteError,
-    onDone,
-  } = useMutation(DELETE_USER)
+  const { mutate: deleteUserMutation, error: deleteError, onDone } = useMutation(DELETE_USER)
 
   // Observadores
-  watch(deleteLoading, (val) => store.setUserLoading(val))
   watch(deleteError, (err) => store.setUserError(err))
   //CallBack cuando la mutation termina
   onDone(({ data }) => {
     // Referenciando al mutation deleteUser(id: $id)
     if (data?.deleteUser) {
-      deleteResult.value = data.deleteUser
       store.setUsers(store.users.filter((u) => u.id !== data.deleteUser.id))
     }
   })
@@ -36,8 +27,6 @@ export default function usePostMutation() {
 
   return {
     // Estados
-    deleteResult,
-    deleteLoading,
     deleteError,
 
     // Acciones
