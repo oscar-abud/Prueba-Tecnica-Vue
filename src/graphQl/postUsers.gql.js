0db71317714f557045603e3f0ad3d7a8846622c4
oscar-abud <oscarpalma.vpr2@gmail.com>
2025-09-03 16:03:50 -0400

    Cnahges: Cambios en la mutation del DELETE_USER

--- src/graphQl/postUsers.gql.js
+++ src/graphQl/postUsers.gql.js
@@ -1,10 +1,8 @@
-import { gql } from '@vue/apollo-composable'
+import { gql } from '@apollo/client/core'
 
 // Eliminamos al usuario por ID
 export const DELETE_USER = gql`
   mutation DeleteUser($id: ID!) {
-    deleteUser(id: $id) {
-      id
-    }
+    deleteUser(id: $id)
   }
 `
