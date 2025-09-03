772fbf548d3c0b3783a937254eac724671bf48d8
oscar-abud <oscarpalma.vpr2@gmail.com>
2025-09-02 22:18:23 -0400

    Add: Incorporacion de Apollo en main.js para que todos puedan acceder a las querys

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
-import { createApp } from 'vue'
+import { createApp, provide, h } from 'vue'
 import { createPinia } from 'pinia'
+import { DefaultApolloClient } from '@vue/apollo-composable'
 
 import App from './App.vue'
 import router from './router'
+import { apolloClient } from './apollo' // tu cliente Apollo
 
-const app = createApp(App)
+const app = createApp({
+  setup() {
+    // Proveer Apollo a todos los composables
+    provide(DefaultApolloClient, apolloClient)
+  },
+  render: () => h(App),
+})
 
 app.use(createPinia())
 app.use(router)
